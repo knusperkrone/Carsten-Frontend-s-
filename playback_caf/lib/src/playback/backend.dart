06c657b2c23d5583f9c25201f3ffddd18e0d53a2
Aron Metzig <aron.metzig@fau.de>
2020-05-29 13:45:41 +0200

    "Fixed" certficiate issue

--- playback_caf/lib/src/playback/backend.dart
+++ playback_caf/lib/src/playback/backend.dart
@@ -1,9 +1,17 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:playback_interop/playback_interop.dart';
-import 'package:http/http.dart' as http;
 
 class BackendAdapter {
+  final client = new HttpClient();
+
+  BackendAdapter() {
+    // Certificate workaround
+    client.badCertificateCallback = (X509Certificate _, String __, int ___) => true;
+  }
+
   Future<String> getVideoId(PlaybackTrack track) async {
     final key = '${track.title} ${track.artist}';
     String id;
@@ -13,18 +21,30 @@ class BackendAdapter {
         '/api/youtube/search',
         {'q': key},
       );
-      final resp = await http.get(uri);
+
+      final request = await client.getUrl(uri);
+      final resp = await request.close();
+      final respBody = await _readResponse(resp);
 
       if (resp.statusCode != 200) {
-        throw new StateError('Invalid status code: ${resp.statusCode}\n${resp.body}');
+        throw new StateError('Invalid status code: ${resp.statusCode}\n$respBody');
       }
-      id = jsonDecode(resp.body)['id'] as String;
+      id = jsonDecode(respBody)['id'] as String;
       if (id == null) {
-        throw new StateError('Invalid id with request ${resp.statusCode}\n${resp.body}');
+        throw new StateError('Invalid id with request ${resp.statusCode}\n$respBody');
       }
     } catch (e) {
       print('[ERROR] couldn\'t get Video id: $id\n$e');
     }
     return id ?? 'QryoOF5jEbc'; // Fallback is twerk
+  }
+
+  Future<String> _readResponse(HttpClientResponse response) {
+    final completer = new Completer<String>();
+    final contents = new StringBuffer();
+    response.transform(utf8.decoder).listen((data) {
+      contents.write(data);
+    }, onDone: () => completer.complete(contents.toString()));
+    return completer.future;
   }
 }
---
+++ playback_caf/test/backend_test.dart
@@ -0,0 +1,11 @@
+import 'package:playback_caf_dart/src/playback/backend.dart';
+import 'package:playback_interop/playback_interop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Fetch youtube id', () async  {
+    final track = new PlaybackTrack.dummy();
+    final resp = await BackendAdapter().getVideoId(track);
+    expect(resp, (String resp) => resp != null && resp == '1SG5A3PYaUs');
+  });
+}
