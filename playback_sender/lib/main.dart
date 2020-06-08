c1b98cdf423b372df7693c86baafb850c66de54f
Aron Metzig <aron.metzig@fau.de>
2020-06-08 21:14:08 +0200

    Only portrait

--- playback_sender/lib/main.dart
+++ playback_sender/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:chrome_tube/ui/pages.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(CarstenApplication());
 
 class CarstenApplication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Carsten',
       theme: _prepareTheme(ThemeData.dark()),
