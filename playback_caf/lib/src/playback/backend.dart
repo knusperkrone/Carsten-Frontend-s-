6084ab334a844fc5114ce96768b65fe45cc4d923
Aron Metzig <aron.metzig@fau.de>
2020-06-08 22:42:38 +0200

    Fixed build issues

--- playback_caf/lib/src/playback/backend.dart
+++ playback_caf/lib/src/playback/backend.dart
@@ -1,17 +1,9 @@
-import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
+import 'package:http/http.dart' as http;
 
 import 'package:playback_interop/playback_interop.dart';
 
 class BackendAdapter {
-  final client = new HttpClient();
-
-  BackendAdapter() {
-    // Certificate workaround
-    client.badCertificateCallback = (X509Certificate _, String __, int ___) => true;
-  }
-
   Future<String> getVideoId(PlaybackTrack track) async {
     final key = '${track.title} ${track.artist}';
     String id;
@@ -22,9 +14,8 @@ class BackendAdapter {
         {'q': key},
       );
 
-      final request = await client.getUrl(uri);
-      final resp = await request.close();
-      final respBody = await _readResponse(resp);
+      final resp = await http.get(uri);
+      final respBody = resp.body;
 
       if (resp.statusCode != 200) {
         throw new StateError('Invalid status code: ${resp.statusCode}\n$respBody');
@@ -36,15 +27,7 @@ class BackendAdapter {
     } catch (e) {
       print('[ERROR] couldn\'t get Video id: $id\n$e');
     }
-    return id ?? 'QryoOF5jEbc'; // Fallback is twerk
-  }
 
-  Future<String> _readResponse(HttpClientResponse response) {
-    final completer = new Completer<String>();
-    final contents = new StringBuffer();
-    response.transform(utf8.decoder).listen((data) {
-      contents.write(data);
-    }, onDone: () => completer.complete(contents.toString()));
-    return completer.future;
+    return id ?? 'QryoOF5jEbc'; // Fallback is twerk
   }
 }
