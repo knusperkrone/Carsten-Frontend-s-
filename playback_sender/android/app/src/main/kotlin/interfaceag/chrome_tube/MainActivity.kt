cf1af338b2a8024de3cf9d3ad27a526c0148982a
Aron Metzig <aron.metzig@fau.de>
2020-10-19 11:38:50 +0200

    Updated android flutter version

--- playback_sender/android/app/src/main/kotlin/interfaceag/chrome_tube/MainActivity.kt
+++ playback_sender/android/app/src/main/kotlin/interfaceag/chrome_tube/MainActivity.kt
@@ -1,40 +1,41 @@
 package interfaceag.chrome_tube
 
-import android.os.Bundle
 import android.view.KeyEvent
-import androidx.core.app.NotificationManagerCompat
+import androidx.annotation.NonNull
 import com.tekartik.sqflite.SqflitePlugin
 import github.showang.flutter_google_cast_button.FlutterGoogleCastButtonPlugin
 import interfaceag.chrome_tube.playback_plugin.CastPlaybackContextPlugin
 import interfaceag.chrome_tube.playback_plugin.service.CastConnectionService
-import io.flutter.app.FlutterActivity
-import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.plugins.util.GeneratedPluginRegister
 import io.flutter.plugins.pathprovider.PathProviderPlugin
 import io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin
 
 
-class MainActivity : FlutterActivity(), PluginRegistry.PluginRegistrantCallback {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity : FlutterActivity(), CastConnectionService.RegistryCallback {
+
+    private val castPlugin = CastPlaybackContextPlugin()
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegister.registerGeneratedPlugins(flutterEngine)
         CastConnectionService.setPluginRegistrant(this)
-        CastPlaybackContextPlugin.registerWith(registrarFor("interfaceag.chrome_tube.cast_plugin.CastPlaybackContextPlugin"))
-        GeneratedPluginRegistrant.registerWith(this)
+        flutterEngine.plugins.add(castPlugin)
     }
 
-    override fun registerWith(registrant: PluginRegistry) {
-        CastPlaybackContextPlugin.registerWith(registrant.registrarFor("interfaceag.chrome_tube.cast_plugin.CastPlaybackContextPlugin"))
-        PathProviderPlugin.registerWith(registrant.registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
-        SharedPreferencesPlugin.registerWith(registrant.registrarFor("io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin"));
-        SqflitePlugin.registerWith(registrant.registrarFor("com.tekartik.sqflite.SqflitePlugin"));
+    override fun registerWith(flutterEngine: FlutterEngine) {
+        flutterEngine.plugins.add(PathProviderPlugin())
+        flutterEngine.plugins.add(SharedPreferencesPlugin())
+        flutterEngine.plugins.add(SqflitePlugin())
+        flutterEngine.plugins.add(castPlugin)
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         var handled: Boolean? = false
         if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
-            handled = CastPlaybackContextPlugin.instance?.sendVolumeDown()
+            handled = castPlugin.sendVolumeDown()
         } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-            handled = CastPlaybackContextPlugin.instance?.sendVolumeUp()
+            handled = castPlugin.sendVolumeUp()
         }
         if (handled == true) {
             return true
@@ -44,17 +45,17 @@ class MainActivity : FlutterActivity(), PluginRegistry.PluginRegistrantCallback 
 
     override fun onResume() {
         super.onResume()
-        CastPlaybackContextPlugin.instance?.onResume()
+        castPlugin.onResume()
         FlutterGoogleCastButtonPlugin.instance?.onResume()
     }
 
     override fun onPause() {
         super.onPause()
-        CastPlaybackContextPlugin.instance?.onPause()
+        castPlugin.onPause()
     }
 
     override fun onDestroy() {
-        CastPlaybackContextPlugin.instance?.onDestroy()
+        castPlugin.onDestroy()
         super.onDestroy()
     }
 }
