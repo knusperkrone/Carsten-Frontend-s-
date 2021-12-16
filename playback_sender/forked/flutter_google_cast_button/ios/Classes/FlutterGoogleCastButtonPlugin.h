df533ffd2d13ccf61e8000572a3f46efd433c135
knusperkrone <aron.metzig@fau.de>
2021-12-16 09:40:37 +0100

    Deleted old dir

--- playback_sender/forked/flutter_google_cast_button/ios/Classes/FlutterGoogleCastButtonPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface FlutterGoogleCastButtonPlugin : NSObject<FlutterPlugin>
-@end
--- playback_sender/forked/flutter_google_cast_button/ios/Classes/FlutterGoogleCastButtonPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterGoogleCastButtonPlugin.h"
-#import <flutter_google_cast_button/flutter_google_cast_button-Swift.h>
-
-@implementation FlutterGoogleCastButtonPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterGoogleCastButtonPlugin registerWithRegistrar:registrar];
-}
-@end
